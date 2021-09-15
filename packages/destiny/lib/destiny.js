4a8f05cd6f8bf1c695e391e94043361162a8dfe3
冯宇林 <fengyulin@bytedance.com>
2021-09-15 12:10:05 +0800

    init

--- packages/destiny/lib/destiny.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = destiny;
-
-function destiny() {
-    // TODO
-}
--- packages/song-of-blaze/lib/SongOfBlaze.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = songofblaze;
-
-function songofblaze() {
-    // TODO
-}
